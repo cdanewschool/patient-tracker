6fc8e3f686e1d61ea21a2d3afd63d67dc81a2bed
damian <bendersd@newschool.edu>
2014-02-21 17:08:12 -0500

    Design updates to charts.

--- www/app/directives/tracker-chart.js
+++ www/app/directives/tracker-chart.js
@@ -78,11 +78,12 @@ app.directive
 								inputEnabled: false
 							},
 							xAxis: {
-								gridLineWidth: 0,
 								lineWidth: (scope.xaxisEnabled!==false?1:0),
-								minorGridLineWidth: (scope.xaxisEnabled!==false?1:0),
-								minorTickWidth: 0,
+								lineColor: '#444',
+								gridLineWidth: (scope.xaxisEnabled!==false?1:0),
+								gridLineColor: '#222',
 								tickWidth: (scope.xaxisEnabled!==false?1:0),
+								tickColor: '#222',
 								type: 'datetime',
 								max: scope.maxDate ? scope.maxDate : null,
 								min: scope.minDate ? scope.minDate : null,	
@@ -105,11 +106,11 @@ app.directive
 								}
 							},
 							yAxis: {
-								gridLineWidth: 0,
-								lineWidth: (scope.xaxisEnabled!==false?1:0),
-								minorGridLineWidth: (scope.yaxisEnabled!==false?1:0),
-								minorTickWidth: 0,
+								lineWidth: (scope.yaxisEnabled!==false?1:0),
+								lineColor: '#444',
+								gridLineWidth: (scope.yaxisEnabled!==false?1:0),
 								tickWidth: (scope.yaxisEnabled!==false?1:0),
+								tickColor: '#222',
 								title: {
 									text: scope.yAxisLabel
 								},
@@ -121,8 +122,13 @@ app.directive
 							plotOptions: {
 								series: {
 									allowPointSelect: true,
+									lineWidth: scope.chartType == constants.CHART_TYPE_SCATTER ? 0 : 4,
+									color: 'rgba(151, 206, 29, .6)',
 									marker: {
-										radius: scope.chartType == constants.CHART_TYPE_SCATTER ? 8 : 3
+										radius: scope.chartType == constants.CHART_TYPE_SCATTER ? 8 : 4,
+										fillColor: '#000',
+										lineWidth: 2,
+										lineColor: null		//inherit from series
 									},
 									point: {
 					                    events: {
